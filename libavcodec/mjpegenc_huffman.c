b0d65e2640b7900010195d7ae5e6f868bdb16f31
wj2ma <williamjma@hotmail.com>
2016-11-13 17:28:52 -0500

    fixed compiler errors and warnings

--- libavcodec/mjpegenc_huffman.c
+++ libavcodec/mjpegenc_huffman.c
@@ -26,23 +26,6 @@
 #include "libavutil/error.h"
 #include "mjpegenc_huffman.h"
 
-typedef struct PTable {
-	int value;
-	int prob;
-} PTable;
-
-typedef struct List {
-	int nitems; 			// number of items in item_idx and probability		ex. 4
-	int item_idx[515];		// index range on the actual items       			    0, 2, 5, 9, 13
-	int probability[514];	// probability of each item                 			3, 8, 18, 46   
-	int items[257 * 16];	// chain of all items									A, B, A, B, C, A, B, C, D, C, D, D, E
-} List;
-
-typedef struct HuffTable {
-	int value;
-	int length;
-} HuffTable;
-
 int compare_by_prob(const void *a, const void *b) {
 	PTable a_val = *(PTable *)a;
 	PTable b_val = *(PTable *)b;
@@ -63,32 +46,31 @@ void buildHuffmanTree(PTable *probTable, HuffTable* distincts, int size) {
 	List *to = malloc(sizeof(List));
 	List *from = malloc(sizeof(List));
 	List *temp;
+
+	int times, i, j, k;
+
+	int nbits[257] = {0};
+
 	to->nitems = 0;
 	from->nitems = 0;
 	to->item_idx[0] = 0;
 	from->item_idx[0] = 0;
-	PTable sorted[size];		// *sorted[i] is the count, sorted[i] - val_counts is the index
-	int i;
-	for (i = 0; i < size; i++) {
-		sorted[i] = probTable[i]; 
-	}
-	qsort(sorted, size, sizeof(PTable), compare_by_prob);
+	qsort(probTable, size, sizeof(PTable), compare_by_prob);
 
-	int times;
 	for (times = 0; times <= 16; times++) {
 		to->nitems = 0;
-	//	from->nitems = 0;
 		to->item_idx[0] = 0;
-	//	from->item_idx[0] = 0;
-		int j = 0, k = 0;
 
-		if (times < 3) { i = 0; }
+		j = 0;
+		k = 0;
+
+		if (times < 16) { i = 0; }
 		while (i < size || j + 1 < from->nitems) {
 			++to->nitems;
 			to->item_idx[to->nitems] = to->item_idx[to->nitems - 1];
-			if (i < size && (j + 1 >= from->nitems || sorted[i].prob < from->probability[j] + from->probability[j + 1])) {
-				to->items[to->item_idx[to->nitems]++] = sorted[i].value;
-				to->probability[to->nitems - 1] = sorted[i].prob;
+			if (i < size && (j + 1 >= from->nitems || probTable[i].prob < from->probability[j] + from->probability[j + 1])) {
+				to->items[to->item_idx[to->nitems]++] = probTable[i].value;
+				to->probability[to->nitems - 1] = probTable[i].prob;
 				++i;
 			} else {
 				for (k = from->item_idx[j]; k < from->item_idx[j + 2]; ++k) {
@@ -103,17 +85,15 @@ void buildHuffmanTree(PTable *probTable, HuffTable* distincts, int size) {
 		from = temp;
 	}
 
-	int nbits[257] = {0};
-
 	for (i = 0; i < from->item_idx[size - 1]; ++i) {
 		++nbits[from->items[i]];
 	}
 
 	// we don't want to return the 256 bit count (it was just in here to prevent all 1s encoding)
-	int j = 0;
+	j = 0;
 	for (i = 0; i < 256; i++) {
 		if (nbits[i] > 0) {
-			distincts[j].value = i;
+			distincts[j].code = i;
 			distincts[j].length = nbits[i];
 			++j;
 		}
@@ -129,8 +109,11 @@ void ff_mjpeg_encode_huffman_init(MJpegEncHuffmanContext *s) {
 
 int ff_mjpeg_encode_huffman_close(MJpegEncHuffmanContext *s,
         uint8_t bits[17], uint8_t val[], int max_nval) {
-    int i;
+    int i, j;
     int nval = 0;
+    PTable *val_counts;
+    HuffTable *distincts;
+
     for (i=0;i<256;++i) {
         if (s->val_count[i])
             ++nval;
@@ -140,8 +123,8 @@ int ff_mjpeg_encode_huffman_close(MJpegEncHuffmanContext *s,
     }
 
 #if 1
-	PTable val_counts[nval + 1];
-	int j = 0;
+	val_counts = malloc((nval + 1)*sizeof(PTable));
+	j = 0;
 	for (i = 0; i < nval; i++) {
 		if (s->val_count[i]) {
 			val_counts[j].value = i;
@@ -150,7 +133,7 @@ int ff_mjpeg_encode_huffman_close(MJpegEncHuffmanContext *s,
 	}
 	val_counts[j].value = 256;
 	val_counts[j].prob = 0;
-	HuffTable distincts[nval];
+	distincts = malloc(nval*sizeof(HuffTable));
 	buildHuffmanTree(val_counts, distincts, nval + 1);
 
 	qsort(distincts, nval, sizeof(HuffTable), compare_by_length);
@@ -179,6 +162,9 @@ int ff_mjpeg_encode_huffman_close(MJpegEncHuffmanContext *s,
         val[i] = i;
     }
 #endif
+
+	free(val_counts);
+	free(distincts);
 
     return 0;
 }

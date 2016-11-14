44f8c52983861a6d21a26d1561006ed7eddc9251
Ted Ying <yingted@gmail.com>
2016-11-14 13:12:27 -0500

    Record huffman encoding test failure

--- libavcodec/tests/mjpegenc_huffman.c
+++ libavcodec/tests/mjpegenc_huffman.c
@@ -33,57 +33,41 @@
 // Test the example given on @see <a
 // href="http://guru.multimedia.cx/small-tasks-for-ffmpeg/">Small Tasks</a>
 int main(int argc, char **argv) {
-    int i;
-    PTable *val_counts;
-    HuffTable *distincts;
-    HuffTable *expected;
-
-    val_counts = malloc(5 * sizeof(PTable));
-    distincts = malloc(5 * sizeof(HuffTable));
-    expected = malloc(5 * sizeof(HuffTable));
-
-    // Init example probability table
-    val_counts[0].value = 0;
-    val_counts[0].prob = 1;
-    val_counts[1].value = 1;
-    val_counts[1].prob = 2;
-    val_counts[2].value = 2;
-    val_counts[2].prob = 5;
-    val_counts[3].value = 3;
-    val_counts[3].prob = 10;
-    val_counts[4].value = 4;
-    val_counts[4].prob = 21;
-
-    // Create expected Huffman Table
-    expected[0].code = 0;
-    expected[0].length = 3;
-    expected[1].code = 1;
-    expected[1].length = 3;
-    expected[2].code = 1;
-    expected[2].length = 3;
-    expected[3].code = 1;
-    expected[3].length = 3;
-    expected[4].code = 0;
-    expected[4].length = 1;
+    int i, ret = 0;
+    // Probabilities of symbols 0..4
+    PTable val_counts[] = {
+        {.value = 0, .prob = 1},
+        {.value = 1, .prob = 2},
+        {.value = 2, .prob = 5},
+        {.value = 3, .prob = 10},
+        {.value = 4, .prob = 21},
+    };
+    // Expected code lengths for each symbol
+    HuffTable expected[] = {
+        {.code = 0, .length = 3},
+        {.code = 1, .length = 3},
+        {.code = 2, .length = 3},
+        {.code = 3, .length = 3},
+        {.code = 4, .length = 1},
+    };
+    // Actual code lengths
+    HuffTable distincts[5];
 
     // Build optimal huffman tree
     ff_mjpegenc_huffman_compute_bits(val_counts, distincts, 5);
 
-    printf("Test");
     for (i = 0; i < 5; i++) {
         if (distincts[i].code != expected[i].code ||
             distincts[i].length != expected[i].length) {
-            printf(
-                "Built huffman does not equal expectations. Expected: code %d "
-                "probability %d, Actual: code %d probability %d",
-                distincts[i].code, distincts[i].length, expected[i].code,
-                expected[i].length);
+            fprintf(stderr,
+                "Built huffman does not equal expectations. "
+                "Expected: code %d probability %d, "
+                "Actual: code %d probability %d\n",
+                expected[i].code, expected[i].length,
+                distincts[i].code, distincts[i].length);
+            ret = 1;
         }
     }
 
-    free(val_counts);
-    free(distincts);
-    free(expected);
-
-    return 0;
+    return ret;
 }
